8880f74db1f1a62d0f1ee03fba1d43a37767d3fe
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-12-21 18:31:53 +0200

    Upgrade skel and make C ext optional.

--- setup.py
+++ setup.py
@@ -14,7 +14,14 @@ from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
 from setuptools import Extension
+try:
+    # Allow installing package without any Cython available. This
+    # assumes you are going to include the .c files in your sdist.
+    import Cython
+except ImportError:
+    Cython = None
 
 
 def read(*names, **kwargs):
@@ -29,6 +36,29 @@ def read(*names, **kwargs):
 # deps have been safely installed).
 if 'TOXENV' in os.environ and 'SETUPPY_CFLAGS' in os.environ:
     os.environ['CFLAGS'] = os.environ['SETUPPY_CFLAGS']
+
+class optional_build_ext(build_ext):
+    """Allow the building of C extensions to fail."""
+    def run(self):
+        try:
+            build_ext.run(self)
+        except Exception as e:
+            self._unavailable(e)
+            self.extensions = []  # avoid copying missing files (it would fail).
+
+    def _unavailable(self, e):
+        print('*' * 80)
+        print('''WARNING:
+
+    An optional code optimization (C extension) could not be compiled.
+
+    Optimizations for this package will not be available!
+        ''')
+
+        print('CAUSE:')
+        print('')
+        print('    ' + repr(e))
+        print('*' * 80)
 
 
 setup(
@@ -78,7 +108,10 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
-    setup_requires=['cython'],
+    setup_requires=[
+        'cython',
+    ] if Cython else [],
+    cmdclass={'build_ext': optional_build_ext},
     ext_modules=[
         Extension(
             splitext(relpath(path, 'src').replace(os.sep, '.'))[0],
@@ -86,6 +119,6 @@ setup(
             include_dirs=[dirname(path)]
         )
         for root, _, _ in os.walk('src')
-        for path in glob(join(root, '*.pyx'))
-    ]
+        for path in glob(join(root, '*.pyx' if Cython else '*.c'))
+    ],
 )
